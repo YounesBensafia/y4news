cc558af258edbeeadc43c950e499d0281f4a6f3b
YounesBensafia <younes.bensafia@etu.usthb.dz>
2025-06-13 23:53:28 +0100

    Done | navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600 mb-4">
-        Welcome to the News App
-      </h1>
-      <p className="text-lg text-gray-700">
-        This is a simple news application built with React and Tailwind CSS.
-      </p>
-    </div>
+    <Navbar />
   );
 };
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Search } from "lucide-react"; // Ensure you have lucide-react installed
+import { FaMoon } from "react-icons/fa"; // Ensure you have react-icons installed
+
+const Navbar = () => {
+  const links = [
+    { name: "Business", path: "/business" },
+    { name: "Entertainment", path: "/entertainment" },
+    { name: "General", path: "/general" },
+    { name: "Health", path: "/health" },
+    { name: "Science", path: "/science" },
+    { name: "Sports", path: "/sports" },
+    { name: "Technology", path: "/technology" },
+  ];
+  return (
+    <nav className="fixed w-full bg-white z-10 shadow-md">
+      <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
+        {/* logo */}
+        <div className="md:text-2xl text-lg font-bold text-blue-600 cursor-pointer hover:text-blue-800 transition duration-400">
+          NewsApp
+        </div>
+        {/* Desktop Links */}
+        <div className="hidden md:flex space-x-4">
+          {links.map((link) => (
+            <a
+              key={link.name}
+              href={link.path}
+              className="text-gray-700 hover:text-blue-600 transition duration-300"
+            >
+              {link.name}
+            </a>
+          ))}
+        </div>
+        <div className="flex items-center justify-center gap-4">
+          <div className="relative bg-gray-200 p-2 rounded-lg">
+            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground h-4 w-4" />
+            <input
+              type="text"
+              placeholder="Search..."
+              className="md:pl-10 pl-7 w-30 md:w-64 outline-none focus:outline-none"
+            />
+          </div>
+          <button className="bg-gray-200 px-3 py-3 rounded-lg cursor-pointer">
+            <FaMoon />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
