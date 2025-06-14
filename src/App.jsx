b35e22e5e01d0944ae319403c7324ba6fdca7306
YounesBensafia <younes.bensafia@etu.usthb.dz>
2025-06-15 00:49:01 +0100

    Add routing to News component and implement API fetching logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,26 @@ import React, { use } from "react";
 import Navbar from "./components/Navbar";
 import News from "./pages/News";
 import { ThemeContext } from "./context/ThemeContext";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   const { theme } = React.useContext(ThemeContext);
   React.useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
   }, [theme]);
   return (
-    <div>
+    <>
       <Navbar />
-      <News />
-    </div>
+      <Routes>
+        <Route path="/" element={<News country="us" category="general" />} />
+        <Route path="/business" element={<News country="us" category="business" />} />
+        <Route path="/entertainment" element={<News country="us" category="entertainment" />} />
+        <Route path="/general" element={<News country="us" category="general" />} />
+        <Route path="/health" element={<News country="us" category="health" />} />
+        <Route path="/science" element={<News country="us" category="science" />} />
+        <Route path="/sports" element={<News country="us" category="sports" />} />
+        <Route path="/technology" element={<News country="us" category="technology" />} />
+      </Routes>
+    </>
   );
 };
 
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+const News = (country, category) => {
+  const fetchAllNews = async () => {
+    try {
+      const response = await fetch(
+        `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching news:", error);
+    }
+  };
+  return <div className="h-screen bg-gray-200 dark:bg-black"></div>;
+};
 
-const News = () => {
-  return (
-    <div className="h-screen bg-gray-200 dark:bg-black">
-
-    </div>
-  )
-}
-
-export default News
+export default News;
