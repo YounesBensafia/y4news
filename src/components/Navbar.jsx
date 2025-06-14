c183dc5d5ec2709a1b559a55b7c84b07de12de02
YounesBensafia <younes.bensafia@etu.usthb.dz>
2025-06-14 19:09:48 +0100

    Add react-router-dom and update Navbar for routing support

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Search } from "lucide-react"; // Ensure you have lucide-react installed
 import { FaMoon } from "react-icons/fa"; // Ensure you have react-icons installed
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
   const links = [
@@ -16,19 +18,19 @@ const Navbar = () => {
     <nav className="fixed w-full bg-white z-10 shadow-md">
       <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
         {/* logo */}
-        <div className="md:text-2xl text-lg font-bold text-blue-600 cursor-pointer hover:text-blue-800 transition duration-400">
-          NewsApp
-        </div>
-        {/* Desktop Links */}
+          <Link to="/" className="md:text-2xl text-lg font-bold text-blue-600 cursor-pointer hover:text-blue-800 transition duration-400">
+            News
+          </Link>
+          {/* Desktop Links */}
         <div className="hidden md:flex space-x-4">
           {links.map((link) => (
-            <a
+            <Link
               key={link.name}
-              href={link.path}
+              to={link.path}
               className="text-gray-700 hover:text-blue-600 transition duration-300"
             >
               {link.name}
-            </a>
+            </Link>
           ))}
         </div>
         <div className="flex items-center justify-center gap-4">
