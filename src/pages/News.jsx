af416712ada2838fd7ef8ac933fdf0a78010d00c
YounesBensafia <younes.bensafia@etu.usthb.dz>
2025-06-15 12:32:25 +0100

    Add loading state to News component with spinner; enhance user experience during news fetching

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from "react";
 import NewsCard from "../components/NewsCard";
+import { Loader2 } from "lucide-react";
 const News = ({ country, category, articles, setArticles }) => {
+  const [loading, setLoading] = React.useState(false);
   const fetchAllNews = async () => {
     try {
+      setLoading(true);
       const response = await fetch(
         `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${
           import.meta.env.VITE_API_KEY
@@ -17,18 +20,27 @@ const News = ({ country, category, articles, setArticles }) => {
     } catch (error) {
       console.error("Error fetching news:", error);
     }
+    finally {
+      setLoading(false);
+    }
   };
   useEffect(() => {
     fetchAllNews();
   }, [country, category]);
   return (
-    <div className="bg-gray-200 dark:bg-black py-24 px-4 md:py-25">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-7">
-        {articles.map((article, index) => (
-          <NewsCard key={index} article={article} />
-        ))}
+    <>
+      {loading && <div className="flex justify-center items-center h-screen">
+        <Loader2 className="h-12 w-12 animate-spin dark:text-gray-200"/>
+        <h1 className="text-gray-800 text-xl font-semibold dark:text-gray-200">Loading...</h1>
+        </div>}
+      <div className="bg-gray-200 dark:bg-black py-24 px-4 md:py-25">
+        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-7">
+          {articles.map((article, index) => (
+            <NewsCard key={index} article={article} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
